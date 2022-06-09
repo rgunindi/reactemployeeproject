2ea04335cd97cde8ebe2a97433ef0af93db8fecf
Ramazan-xcod3r <codmyhesap@gmail.com>
2022-06-09 17:40:25 +0300

    admin page added

--- server/server.js
+++ server/server.js
@@ -15,11 +15,25 @@ app.get('/workareas',(req,res)=>{
         });
     });
 })
+app.get('/admins',(req,res)=>{
+    const AdminQUERY = `USE Workers;
+    SELECT employees.employee_id,first_name, last_name, salary,department_id,hire_date,to_date
+    FROM employees, salaries
+    WHERE employees.employee_id = salaries.employee_id 
+    GROUP BY salaries.employee_id`;
+    connection.query(AdminQUERY,(err,results)=>{
+        if(err){
+            return res.send(err);
+        }
+        return res.json({
+            data:results
+        });
+    });
+})
 
 
 app.post('/addEmployee',(req,res)=>{
     const ADD_QUERY = `INSERT INTO task VALUES ('${req.body.task}')`;
-
 }
 )
 app.get('/deleteEmployee',(req,res)=>{
